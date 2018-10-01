6ea55db43664bc12854a8b607a038b7bb0c77dc7
Marek Wawreniuk <marek.wawreniuk@websensa.com>
2018-10-01 10:48:04 +0200

    Tweak eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,19 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/recommended',
+    'standard'
+  ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "always"
+    }]
   },
   parserOptions: {
     parser: "babel-eslint"
