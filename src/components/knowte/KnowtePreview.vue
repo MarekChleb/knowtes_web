f4c85ef58133e7e4b86f50e7adbe9bf04e92521e
Marek Wawreniuk <marek.wawreniuk@websensa.com>
2018-10-01 10:48:11 +0200

    WIP

---
+++ src/components/knowte/KnowtePreview.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-flex
+    ":sizeName"
+  >
+    sda
+  </v-flex>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: Number,
+      default: 3
+    }
+  },
+
+  computed () {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <the-knowtest/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import TheKnowtes from '@/components/TheKnowtes.vue'
 
 export default {
-  name: "home",
+  name: 'Home',
   components: {
-    HelloWorld
+    TheKnowtes
   }
-};
+}
 </script>
